5bc47f6207e452c5b6b47aa537fbb52e5faca698
kevella95 <gisellaanindyaaisha.sdigs@gmail.com>
2026-06-02 16:26:30 +0700

    revisi 3

--- UjianUKL2/Program.cs
+++ UjianUKL2/Program.cs
@@ -1,97 +1,100 @@
-﻿List<Stand> data_Stand = new List<Stand>();
-
+﻿class Program
 {
-    new StandOudoor("Stand Outdoor 1", 400000);
-    new StandOudoor("Stand Outdoor 2", 500000);
-    new StandIndoor("Stand Indoor 1", 700000);
-    new StandIndoor("Stand Indoor 2", 800000);
-    new StandPremium("Stand Premium 1", 1800000);
-    new StandPremium("Stand Premium 2", 2000000);
+    static void Main()
+    {
+
+
+
+        List<Stand> data_Stand = new List<Stand>();
+            data_Stand.Add(new StandOutdoor("Stand Outdoor 1", 400000));
+            data_Stand.Add(new StandOutdoor("Stand Outdoor 2", 500000));
+            data_Stand.Add(new StandIndoor("Stand Indoor 1", 700000));
+            data_Stand.Add(new StandIndoor("Stand Indoor 2", 800000));
+            data_Stand.Add(new StandPremium("Stand Premium 1", 1800000));
+            data_Stand.Add(new StandPremium("Stand Premium 2", 2000000));
+
+        while (true)
+        {
+            Console.WriteLine("===Moklet Expo Management Center===");
+            Console.WriteLine("\nDaftar Stand");
+            for (int i = 0; i < data_Stand.Count; i++)
+            {
+                Console.WriteLine($"{data_Stand[i].NamaStand,-15} | {data_Stand[i].HargaSewaPerhari,-15} | {data_Stand[i].IsAvailable,-15} ");
+            }
+
+
+            Console.WriteLine("1. sewa\n2.kembali\n3. keluar");
+            Console.Write("Pilih menu: ");
+            string pilihan = Console.ReadLine();
+
+            if (pilihan == "1")
+            {
+                Console.Write("\nInput nama: ");
+                string? nama_Stand = Console.ReadLine();
+
+                var cari_Stand = data_Stand.FirstOrDefault(cr => cr.NamaStand.ToLower() == nama_Stand?.ToLower());
+
+                if (cari_Stand == null)
+                {
+                    Console.WriteLine("Stand tidak di temukan");
+                }
+
+                else if (cari_Stand.IsAvailable)
+                {
+                    Console.Write("\nInput jumlah hari sewa");
+                    int hari = int.Parse(Console.ReadLine());
+
+                    double totalBiaya = cari_Stand.HitungTotal(hari);
+
+                    cari_Stand.UbahStatus();
+
+                    Console.WriteLine($"Total pembayaran sewa: Rp {totalBiaya}");
+                }
+
+
+
+            }
+            if (pilihan == "2")
+            {
+                Console.WriteLine("\nDaftar Stand:");
+                string namaStand = Console.ReadLine();
+
+                var cari_Stand = data_Stand.FirstOrDefault(cr => cr.NamaStand.ToLower() == namaStand?.ToLower());
+
+                if (cari_Stand == null)
+                {
+                    Console.WriteLine("Stand tidak ditemukan");
+                }
+
+                else if (!cari_Stand.IsAvailable)
+                {
+                    cari_Stand.UbahStatus();
+
+                    Console.WriteLine("\nStand berhasil dikembalikan!");
+                }
+                else
+                {
+                    Console.WriteLine("\nProses pengembalian tidak bisa dilakukan");
+                }
+
+            }
+
+
+            else if (pilihan == "3")
+            {
+                Console.WriteLine("Terima Kasih");
+                break;
+            }
+
+            else
+            {
+                Console.WriteLine("\nPilihan invalid");
+            }
+            Console.WriteLine("Tekan ENTER untuk menutup Aplikasi....");
+            Console.ReadLine();
+        }
+    }
 }
-
-while (true)
-{
-    Console.WriteLine("===Moklet Expo Management Center===");
-    Console.WriteLine("\nDaftar Stand");
-    foreach (var ds in data_Stand )
-    {
-        ds.TampilkanInfo();
-    }
-
-    
-    Console.WriteLine("1. sewa\n2.kembali\n3. keluar");
-    Console.Write("Pilih menu: ");
-    string pilihan = Console.ReadLine();
-
-    if (pilihan == "1")
-    {
-        Console.Write("\nInput nama Stand");
-        string nama_Stand = Console.ReadLine();
-
-        var cari_Stand = data_Stand.FirstOrDefault(cr => string.Equals(cr.NamaStand, nama_Stand, StringComparison.OrdinalIgnoreCase));
-
-        if (cari_Stand == null)
-        {
-            Console.WriteLine("Stand tidak di temukan");
-        }
-
-        else if (cari_Stand.IsAvailable)
-        {
-            Console.Write("\nInput jumlah hari sewa");
-            int hari = int.Parse(Console.ReadLine());
-
-            double totalBiaya = cari_Stand.HitungTotal(hari);
-
-            cari_Stand.UbahStatus();
-
-            Console.WriteLine($"Total pembayaran sewa: Rp {totalBiaya}");
-        }
-
-
-
-    }
-    if (pilihan == "2")
-    {
-        Console.WriteLine("\nDaftar Stand:");
-        string namaStand = Console.ReadLine();
-
-        var cari_Stand = data_Stand.FirstOrDefault(cr => string.Equals(cr.NamaStand, namaStand, StringComparison.OrdinalIgnoreCase));
-
-
-        if (cari_Stand == null)
-        {
-            Console.WriteLine("Stand tidak ditemukan");
-        }
-
-        else if (!cari_Stand.IsAvailable)
-        {
-            cari_Stand.UbahStatus();
-
-            Console.WriteLine("\nStand berhasil dikembalikan!");
-        }
-        else
-        {
-            Console.WriteLine("\nProses pengembalian tidak bisa dilakukan");
-        }
-
-    }
-
-
-    else if (pilihan == "3")
-    {
-        Console.WriteLine("Terima Kasih");
-        break;
-    }
-
-    else
-    {
-        Console.WriteLine("\nPilihan invalid");
-    }
-    Console.WriteLine("Tekan ENTER untuk menutup Aplikasi....");
-    Console.ReadLine();
-}
-
-
 class Stand
 {
     protected string _namaStand;
@@ -109,7 +112,12 @@ class Stand
     public string NamaStand
     {
         get { return _namaStand; }
-        set { _namaStand = value; }
+        set { 
+            if (!string.IsNullOrWhiteSpace(value) || !string.IsNullOrEmpty(value))
+            {
+                _namaStand = value; 
+            }
+        }
     }
 
     public double HargaSewaPerhari
@@ -175,11 +183,11 @@ class StandIndoor : Stand
 }
 
 
-class StandOudoor : Stand
+class StandOutdoor : Stand
 {
     private double _biayaTenda;
 
-    public StandOudoor(string namaStand, double hargaSewaPerhari) : base(namaStand, hargaSewaPerhari)
+    public StandOutdoor(string namaStand, double hargaSewaPerhari) : base(namaStand, hargaSewaPerhari)
     {
         _biayaTenda = 75000;
     }
